e95a0c533a13a3f62308dd7f73c92869038dc1ad
mefels2744 <76845941+mefels2744@users.noreply.github.com>
2022-01-17 23:05:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global . = [ '6285609723473' ,    ' 6285609723473' , '6285609723473' ] // pemilik memasukkan nomor Anda di sini
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
